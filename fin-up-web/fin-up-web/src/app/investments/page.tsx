9aa58155343ca49b4650658d17e309307da45cab
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-26 11:38:46 -0300

    dashboard

--- fin-up-web/fin-up-web/src/app/investments/page.tsx
+++ fin-up-web/fin-up-web/src/app/investments/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "@/components/nav-bar";
 export default function InvestmentsPage(){
     return(
         <>
-           <NavBar active="investmentos" />
+           <NavBar active="investimentos" />
 
             <main className="flex justify-center">
                 <div className="bg-slate-900 min-w-2/3 m-6 p-6 rounded">
